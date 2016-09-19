a6c9bb25c0f766e0de25df23dffc797f09bc2b1f
Stanley Pan <stanleypan.egre@gmail.com>
2016-09-20 00:45:29 +0900

    Add FeedCell and protocol methods

--- MeBook/ViewController.swift
+++ MeBook/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class FeedController: UICollectionViewController {
+let cellId = "cellId"
+
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Facebook Feed"
         
+        collectionView?.alwaysBounceVertical = true
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
+
+class FeedCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .white
     }
 }
